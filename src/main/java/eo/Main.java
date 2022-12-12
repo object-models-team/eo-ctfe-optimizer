f1313c4b5c35644b30c503319a70f77cb881a177
Ruslan Garifullin <ruslan.g2002@gmail.com>
2022-12-12 14:10:49 +0300

    feat: add Oracle XML Parser

---
+++ src/main/java/eo/Main.java
@@ -0,0 +1,9 @@
+package eo;
+
+import oracle.xml.parser.v2.XSLProcessor;
+
+public class Main {
+    public static void main(String[] args) {
+        XSLProcessor.XSLTVersion xslt20 = XSLProcessor.XSLT20;
+    }
+}
